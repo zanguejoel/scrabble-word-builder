bb1c699faef13c8ae0bc725ce8b6e0bafc998b21
Joel <joel@neptunetech.io>
2026-01-27 21:19:15 -0500

    bug: removed unused variable

--- src/components/DictionaryDrawer.tsx
+++ src/components/DictionaryDrawer.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState, UIEvent } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 interface DictionaryDrawerProps {
   open: boolean;
